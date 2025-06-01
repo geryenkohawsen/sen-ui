dfbc96570c62d7e2397fc4e0be900cf32efc37a2
Gery <gery.zarons@gmail.com>
2025-06-02 01:17:37 +0900

    feat: add Tailwind CSS configuration file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,8 @@ import type { Config } from 'tailwindcss'
 
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
 } satisfies Config
