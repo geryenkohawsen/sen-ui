2b036c7f4f0e108a0659aba7a59d7d55ace6683c
Gery <gery.zarons@gmail.com>
2025-06-02 01:26:40 +0900

    fix: remove commented-out WIP elements in PageTop component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,6 @@
 export default function PageTop() {
   return (
     <div className="m-auto">
-      {/* <div className="animate-slidein opacity-0 [--slidein-delay:300ms]">
-        <p className="text-center text-9xl font-extrabold text-white/95">WIP</p>
-      </div>
-      <div className="animate-slidein opacity-0 [--slidein-delay:500ms]">
-        <p className="text-center text-9xl font-extrabold text-white/95">WIP</p>
-      </div>
-      <div className="animate-slidein opacity-0 [--slidein-delay:700ms]">
-        <p className="text-center text-9xl font-extrabold text-white/95">WIP</p>
-      </div> */}
       <p className="flex flex-col gap-8 text-9xl font-extrabold text-slate-100/95 lg:flex-row">
         <span className="animate-slidein opacity-0 [--slidein-delay:300ms]">
           WIP
