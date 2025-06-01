4e8b12974f952925e26c351bbe00fd23486cddca
Gery <gery.zarons@gmail.com>
2025-06-02 00:30:53 +0900

    fix: simplify layout by removing unnecessary div wrappers in PageTop component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
 export default function PageTop() {
   return (
-    <div className="flex h-[100dvh] flex-col items-center justify-center gap-6 p-6">
-      <div className="overflow-hidden">
-        <p className="animate-pulse bg-gradient-to-r from-[#4eb2cb] via-[#2f89a4] to-[#22667f] bg-clip-text text-center text-9xl font-extrabold tracking-wide text-ellipsis text-transparent">
-          WIP WIP WIP
-        </p>
-      </div>
-    </div>
+    <p className="my-auto animate-pulse bg-gradient-to-r from-[#4eb2cb] via-[#2f89a4] to-[#22667f] bg-clip-text text-center text-9xl font-extrabold tracking-wide text-ellipsis text-transparent">
+      WIP WIP WIP
+    </p>
   )
 }
