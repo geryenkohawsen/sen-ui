8627b1cef1209a45828e7d8f01a5241e26938443
Gery <gery.zarons@gmail.com>
2025-06-02 01:17:52 +0900

    fix: refactor PageTop component to use spans for WIP text with slide-in animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
 export default function PageTop() {
   return (
-    <p className="my-auto text-center text-9xl font-extrabold text-white/95">
-      WIP WIP WIP
-    </p>
+    <div className="m-auto">
+      {/* <div className="animate-slidein opacity-0 [--slidein-delay:300ms]">
+        <p className="text-center text-9xl font-extrabold text-white/95">WIP</p>
+      </div>
+      <div className="animate-slidein opacity-0 [--slidein-delay:500ms]">
+        <p className="text-center text-9xl font-extrabold text-white/95">WIP</p>
+      </div>
+      <div className="animate-slidein opacity-0 [--slidein-delay:700ms]">
+        <p className="text-center text-9xl font-extrabold text-white/95">WIP</p>
+      </div> */}
+      <p className="flex flex-col gap-8 text-9xl font-extrabold text-slate-100/95 lg:flex-row">
+        <span className="animate-slidein opacity-0 [--slidein-delay:300ms]">
+          WIP
+        </span>
+        <span className="animate-slidein opacity-0 [--slidein-delay:500ms]">
+          WIP
+        </span>
+        <span className="animate-slidein opacity-0 [--slidein-delay:700ms]">
+          WIP
+        </span>
+      </p>
+    </div>
   )
 }
